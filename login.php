ae9f592dd6efc85d91bbe154ba974da751194ac9
梶浦　歩 <nanbu370@gmail.com>
2024-10-21 10:16:32 +0900

    ログイン確認

--- login.php
+++ login.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>ログイン確認</title>
+</head>
+<body>
+    <h1>ログイン確認</h1>
+    <?php
+        
+    ?>
+</body>
+</html>
+
 <?
 
 php?>
