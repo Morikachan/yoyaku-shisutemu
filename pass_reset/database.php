064d7266f74cc331dbd8d5346994ef5b8f46a2cc
haruto-sugihara <sugihr-528@yahoo.co.jp>
2024-10-30 13:42:11 +0900

    パスワードのリセットのシステムを更新しました

---
+++ pass_reset/database.php
@@ -0,0 +1,19 @@
+<?php
+//データベース情報
+const DB_SERVER_NAME = 'localhost';
+const DB_USER_NAME = 'root';
+const DB_PASSWORD = '';
+const DB_NAME = 'reservationsystem_db';
+//ここまでデータベース情報
+function getDb(){
+        try {
+            $pdo = new PDO("mysql:host=" . DB_SERVER_NAME .
+            ";dbname=" . DB_NAME, DB_USER_NAME, DB_PASSWORD);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $pdo;
+        } catch (PDOException $e) {
+            echo '接続失敗' . $e->getMessage();
+            exit();
+        }
+    }
+?>
--- pass_reset/new_pass/new_pass.php
+++ pass_reset/new_pass/new_pass.php
@@ -1,3 +1,46 @@
 <?php
-$newpass = $_POST['reset_pass1'];
+//session利用開始
+session_start();
+$mail = $_SESSION['mail'];
+$newpass = $_POST['reset_pass'];
+$repeatpass = $_POST['repeat_pass'];
+if($newpass == "" || $repeatpass == ""){
+    echo 'パスワードを入力してください。';
+} else {
+    if($newpass != $repeatpass){
+        $alert = "<script type='text/javascript'>alert('同じパスワードを入力してください。');</script>";
+        echo $alert;
+    } else {
+        //データベースのデータの参照
+        require_once '../database.php';
+        $pdo = getDb();
+
+        //パスワードのリセット処理
+        function passreset($pdo , $mail, $newpass){
+            $sql = "UPDATE users_info SET passwd = :newpass WHERE mail = :mail";
+            $sql = "SELECT * FROM users_info WHERE mail = :mail ";
+            try{
+                //SQL文に入れる値の設定
+                $stmt = $pdo->prepare($sql);
+                $stmt -> bindParam(":mail" , $mail);
+                $stmt -> bindParam(":newpass" , $newpass);
+                $stmt -> execute();
+                // return $stmt -> execute(); // true or false
+                $rowsAffected = $stmt -> rowCount();
+                return $rowsAffected > 0;
+            } catch (PDOException $e){
+                echo $e->getMessage();
+                return false;
+            }
+            
+        }   
+        $result = passreset($pdo,$mail,$newpass);
+
+        if($result){
+            echo 'パスワードの変更が完了しました。';
+        } else {
+            echo "パスワードのリセットに失敗しました \n 何回も続く場合はお問い合わせ画面よりお問い合わせください";
+        }
+    }
+}
 ?>
