5f3ad3638214e62186ae2bd7b7ebcde543921b3d
✿ オレクサンドラ ✿ <zuzukochkame@gmail.com>
2024-11-05 13:58:42 +0900

    モーダルウィンドウの始まり

--- registartion/registration.php
+++ registartion/registration.php
@@ -25,24 +25,6 @@ $address2 = $_POST['address2'];
 session_start();
 $_SESSION['mail'] = $mail;
 
-
-const DB_SERVER_NAME = 'localhost';
-const DB_USER_NAME = 'root';
-const DB_PASSWORD = '';
-const DB_NAME = 'reservationsystem_db';
-
-function getDbConnection() {
-    try {
-        $pdo = new PDO("mysql:host=" . DB_SERVER_NAME .
-        ";dbname=" . DB_NAME, DB_USER_NAME, DB_PASSWORD);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        return $pdo;
-    } catch (PDOException $e) {
-        echo '接続失敗' . $e->getMessage();
-        exit();
-    }
-}
-
 function insertStudentData($pdo, $name, $password){
     $sql = "INSERT INTO users_info (mail, password) VALUES (:mail, :password)";
     try{
