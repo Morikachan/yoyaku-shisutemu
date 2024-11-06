bbf0e9d09af86cacf7ed815c3787eaa1f1b9f636
✿ オレクサンドラ ✿ <zuzukochkame@gmail.com>
2024-11-06 12:29:10 +0900

    modal windowの調整、DBにあるメール確認

--- registartion/modal-window.js
+++ registartion/modal-window.js
@@ -12,24 +12,14 @@ function insertConfirm() {
       })
 }
 
-
 // Получить модальный
-var modal = document.getElementById("myModal");
+let modal = document.getElementById("modal");
 // Получить кнопку, которая открывает модальный
-var btn = document.getElementById("myBtn");
-// Получить элемент <span>, который закрывает модальный
-var span = document.getElementsByClassName("close")[0];
-// Когда пользователь нажимает на кнопку, откройте модальный
+let btn = document.getElementById("modalBtn");
+
 btn.onclick = function() {
   modal.style.display = "block";
 }
-// Когда пользователь нажимает на <span> (x), закройте модальное окно
 span.onclick = function() {
   modal.style.display = "none";
-}
-// Когда пользователь щелкает в любом месте за пределами модального, закройте его
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
 }
--- registartion/registration.php
+++ registartion/registration.php
@@ -1,36 +1,10 @@
 <?php
-$mail = $_POST['mail'];
-$password = $_POST['password'];
-$course = $_POST['course'];
-
-// 名前
-$lastName = $_POST['lastName'];
-$firstName = $_POST['firstName'];
-$lastNameKana = $_POST['lastNameKana'];
-$firstNameKana = $_POST['firstNameKana'];
-
-$gender = $_POST['gender'];
-$date = $_POST['date'];
-$formattedDate = date("Y/n/j", strtotime($date)). "<br/>";
-
-$occupation = $_POST['occupation'];
-$school = $_POST['school'];
-$tel = $_POST['tel'];
-
-// 住所
-$zipcode = $_POST['zipcode'];
-$address1 = $_POST['address1'];
-$address2 = $_POST['address2'];
-
-session_start();
-$_SESSION['mail'] = $mail;
-
-function insertStudentData($pdo, $name, $password){
-    $sql = "INSERT INTO users_info (mail, password) VALUES (:mail, :password)";
+require_once '../core/Database.php';
+function searchMail($pdo, $mail){
+    $sql = "SELECT FROM users_info WHERE mail=:mail";
     try{
         $smtp = $pdo->prepare($sql);
-        $smtp->bindParam(':name', $name);
-        $smtp->bindParam(':password', $password);
+        $smtp->bindParam(':mail', $mail);
         return $smtp->execute();
     } catch (PDOException $e){
         echo $e->getMessage();
@@ -38,12 +12,39 @@ function insertStudentData($pdo, $name, $password){
     }
 }
 
-$pdo = getDbConnection();
-$results = true;
-// $results = insertStudentData($pdo, $name, $password);
-if($results){
-    header("Location: registration-confirm.php");
-} else {
-    echo '登録失敗しました';
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $mail = $_POST['mail'];
+    $password = $_POST['password'];
+    $course = $_POST['course'];
+    // 名前
+    $lastName = $_POST['lastName'];
+    $firstName = $_POST['firstName'];
+    $lastNameKana = $_POST['lastNameKana'];
+    $firstNameKana = $_POST['firstNameKana'];
+
+    $gender = $_POST['gender'];
+    $date = $_POST['date'];
+    $formattedDate = date("Y/n/j", strtotime($date)). "<br/>";
+
+    $occupation = $_POST['occupation'];
+    $school = $_POST['school'];
+    $tel = $_POST['tel'];
+
+    // 住所
+    $zipcode = $_POST['zipcode'];
+    $address1 = $_POST['address1'];
+    $address2 = $_POST['address2'];
+
+    session_start();
+    $_SESSION['mail'] = $mail;
+    $_SESSION['password'] = $password;
+    $_SESSION['course'] = $course;
+    
+    // Checking mail
+    $DB = new Database();
+    $pdo = $DB->getPDO();
+    $isMailExist = searchMail($pdo, $mail);
+    $isMailExist ? header("Location: registration.html") /*Error email already exist*/ 
+    : header("Location: registration-confirm.html");
 }
 ?>
