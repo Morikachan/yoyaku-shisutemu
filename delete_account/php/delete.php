d06787c4ddcd38d932f526d13b6e06097c2c2851
takuto-takuto <tchitian360@gmail.com>
2024-11-11 09:40:04 +0900

    delete.phpを直しました

--- delete_account/php/delete.php
+++ delete_account/php/delete.php
@@ -51,67 +51,6 @@ function deletappoint($pdo , $mail){
 }
 
 session_start();
-$mail = $_SESSION['mail'];
-$passwd = $_SESSION['passwd'];
-
-$pdo = getDBConnection();
-
-$result = deletData($pdo , $mail , $passwd);
-if($result){
-    $pdo = getDBConnection();
-    $mail = $_SESSION['mail'];
-    $result = deletappoint($pdo , $mail);
-    if($result){<?php
-const DB_SERVER_NAME = 'localhost';
-const DB_USER_NAME = 'root';
-const DB_PASSWORD = '';
-const DB_NAME = 'reservationsystem_db';
-
-function getDBConnection() {
-    try{
-        $pdo = new PDO("mysql:host=" . DB_SERVER_NAME . ";dbname=" . DB_NAME , DB_USER_NAME, DB_PASSWORD);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-        return $pdo;
-    }
-    catch(PDOException $e) {
-        echo '接続失敗' , $e->getMessage();       
-        exit(); 
-    }
-}
-
-function deletData($pdo , $mail , $passwd){
-    
-    $sql = 'DELETE FROM users_info WHERE mail = :mail';
-
-    try{
-        $stmt = $pdo->prepare($sql);
-        $stmt->bindParam(':mail' , $mail);
-        //
-        return $stmt->execute(); 
-    }
-    catch (PDOException $e){
-        echo $e->getMessage();
-        return false;
-    }
-}
-function deletappoint($pdo , $mail){
-    $sql = "DELETE FROM appointment WHERE mail = :mail";
-
-    try{
-        $stmt = $pdo->prepare($sql);
-        $stmt->bindParam(':mail' , $mail);
-        //
-        $stmt->execute();
-        $rowsAffected = $stmt->rowCount();
-        return $rowsAffected > 0;
-    }
-    catch (PDOException $e){
-        echo $e->getMessage();
-        return false;
-    }
-}
-
-session_start();
 $pdo = getDBConnection();
 $mail = $_SESSION['mail'];
 $passwd = $_SESSION['passwd'];
