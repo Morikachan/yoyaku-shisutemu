a4e93f13634ac95fa8adc6ddc0e62e701e139629
haruto-sugihara <sugihr-528@yahoo.co.jp>
2024-11-15 09:43:11 +0900

    ハンバーガメニューをスクロールできてしまう問題を修正、しかし、ウインドウの大きさを変えたときにheaderが消えることがあるため改良の余地あり。

--- login.js
+++ login.js
@@ -2,15 +2,18 @@ function hamburgerClick(){
     const elem = document.getElementById("hamburger-btn");
     const hamburger_menu_list = document.getElementById("hamburger-menu_list");
     const main = document.getElementById("main");
+    const body = document.getElementById("body");
     
    if(hamburger_menu_list.classList.contains('active')){
         hamburger_menu_list.classList.remove('active');
         main.classList.remove('active');
+        body.classList.remove('active');
         elem.classList.remove('close');
         elem.classList.add('open');
    } else {
         hamburger_menu_list.classList.add('active');
         main.classList.add('active');
+        body.classList.add('active');
         elem.classList.remove('open');
         elem.classList.add('close');
    }
