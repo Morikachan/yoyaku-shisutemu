8d1994bc770ab79a66760a2f3fc3cdad664507a3
✿ オレクサンドラ ✿ <zuzukochkame@gmail.com>
2024-11-25 12:45:23 +0900

    新規登録のバリデーションとバリデーションエラー表示の調整

--- registartion/registration-confirm.php
+++ registartion/registration-confirm.php
@@ -14,19 +14,45 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="./registration_style.css">
+    <script src="../login.js"></script>
     <title>新規登録確認</title>
 </head>
 <body>
-    <header>
-        <div class="header-wrap">
-            <p><img src="../img/image 1.png" alt="Arts_Logo"></p>
-            <p><a href="#" class="red-button">新規登録</a></p>
-            <div/>
-            <div class="nav-wrap">
-                <!-- ハンバーガーメニュー部分 -->
+    <header class="c-header c-hamburger-menu">
+
+            <!-- アーツカレッジヨコハマのロゴ -->
+            <div class="flex_logo">
+                <a href="https://www.kccollege.ac.jp/" class="c-header__logo"><img src="../img/image 1.png" alt="Arts_Logo"></a>
+            </div>
+
+            <!-- ロゴを除くオブジェクトを右に固定するためのdiv -->
+            <div class="flex_header">    
                 
-                <!--ここまででハンバーガメニュー終わり-->
-            </header>
+                  <!-- ハンバーガメニューのリスト -->
+                  <ul class="c-header__list c-hamburger-menu__list" id="hamburger-menu_list"><!-- 追記 クラスを追記 -->
+                      <li class="c-header__list-item">
+                        <a href="https://www.kccollege.ac.jp/" class="c-header__list-link">ホームページへ</a>
+                      </li>
+                      <li class="c-header__list-item">
+                        <a href="#" class="c-header__list-link">参加履歴</a>
+                      </li>
+                      <li class="c-header__list-item">
+                        <a href="#" class="c-header__list-link">登録内容の変更</a>
+                      </li>
+                      <li class="c-header__list-item">
+                        <a href="#" class="c-header__list-link">アカウント削除</a>
+                      </li>
+                      <li class="c-header__list-item">
+                        <a href="#" class="c-header__list-link">お問い合わせ</a>
+                      </li>
+                  </ul>
+                  
+                  <!-- 新規登録ボタン -->
+                  <a href="#" class="red-button">新規登録</a>
+                  <!-- ハンバーガボタン -->
+                  <div id="hamburger-btn" class="open" onclick="hamburgerClick()"></div>
+            </div>
+    </header>
             <main>
                 <h1 id="h1">新規登録確認</h1>
                 <div class="content-container">
