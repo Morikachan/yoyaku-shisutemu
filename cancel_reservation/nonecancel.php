cbb525e9f2495d4d68756fe7c99baf480b7a7838
takuto-takuto <tchitian360@gmail.com>
2024-12-03 14:51:39 +0900

    nonecancel.phpを改修しました

--- cancel_reservation/nonecancel.php
+++ cancel_reservation/nonecancel.php
@@ -12,89 +12,74 @@ $results = $_SESSION['results'];
         <title>予約キャンセル</title>
     </head>
     <body>
-        <script src="../hamburger.js"></script>
         <header>
             <div class="header-wrap">
                 <img src="../img/image 1.png" alt="Arts_Logo">
             </div>
-            <!-- ロゴを除くオブジェクトを右に固定するためのdiv -->
-            <div class="flex_header">    
+            <div class="header-wrap">
                 
-                  <!-- ハンバーガメニューのリスト -->
-                  <ul class="c-header__list c-hamburger-menu__list" id="hamburger-menu_list"><!-- 追記 クラスを追記 -->
-                      <li class="c-header__list-item">
-                        <a href="https://www.kccollege.ac.jp/" class="c-header__list-link">ホームページへ</a>
-                      </li>
-                      <li class="c-header__list-item">
-                        <a href="#" class="c-header__list-link">参加履歴</a>
-                      </li>
-                      <li class="c-header__list-item">
-                        <a href="#" class="c-header__list-link">登録内容の変更</a>
-                      </li>
-                      <li class="c-header__list-item">
-                        <a href="#" class="c-header__list-link">アカウント削除</a>
-                      </li>
-                      <li class="c-header__list-item">
-                        <a href="#" class="c-header__list-link">お問い合わせ</a>
-                      </li>
-                  </ul>
-                  
-                  <!-- 新規登録ボタン -->
-                  <a href="#" class="red-button">新規登録</a>
-                  <!-- ハンバーガボタン -->
-                  <div id="hamburger-btn" class="open" onclick="hamburgerClick()"></div>
+                <!-- ハンバーガーメニュー部分 -->
+                
+                <!--ここまででハンバーガメニュー終わり-->
             </div>
         </header>
-
         <main>
             <p class="locatoin"><a href="">ホームページ</a>へ/<a href="../mypage/mypage.php">マイページ</a>へ/予約キャンセル</p>
             <h1>予約キャンセル</h1>
             <div class="content-container">
-                <div class="error error2">
-                    <p>・予約日が選択されていません</p>
-                    <p>　もう一度やり直してください。</p>
-                </div>
+                <?php if($results): ?>
+                    <div class="error error2">
+                        <p>・予約日が選択されていません</p>
+                        <p>　もう一度やり直してください。</p>
+                    </div>
+                <?php endif; ?>
                 <h2>予約情報</h2>
                 <div class="Reservation">
-                    <h3>・現在の予約状況</h3>
-                    <form action="./check_cancel.php" method="post">
-                        <table border="1">
-                            <tr>
-                                <thead>
-                                    <th>No.</th>
-                                    <th>予約日</th>
-                                    <th>予約時間</th>
-                                    <th>キャンセル</th>
-                                </thead>
-                            </tr>
-                            <?php $counter = 1;foreach ($results as $row):?>
+                    <?php if($results): ?>
+                        <h3>・現在の予約状況</h3>
+                        <form action="./check_cancel.php" method="post">
+                            <table border="1">
                                 <tr>
-                                    <th><?= $counter; ?></th>
-                                    <th><?= htmlspecialchars($row['day']); ?></th>
-                                    <th><?= htmlspecialchars($row['time']); ?>時～</th>
-                                    <th>
-                                        <input type="checkbox" name="selected[]" value="<?= $counter; ?>">
-                                        <input type="hidden" name="day_<?= $counter; ?>" value="<?= htmlspecialchars($row['day']); ?>">
-                                        <input type="hidden" name="time_<?= $counter; ?>" value="<?= htmlspecialchars($row['time']); ?>">
-                                    </th>
-                                    
+                                    <thead>
+                                        <th>No.</th>
+                                        <th>予約日</th>
+                                        <th>予約時間</th>
+                                        <th>キャンセル</th>
+                                    </thead>
                                 </tr>
-                            <?php $counter++; endforeach; ?>
-                        </table>
-                        <p class="error-font error-font2">※キャンセルしたい予約日を選択してください</p>
-                        <h3>・本人確認</h3>
-                        <div class="check-zone">
-                            <p>
-                                <h4>・メールアドレス</h4>
-                                <input type="email" name="mail" class="check">
-                            </P>
-                            <p>
-                                <h4>・パスワード</h4>
-                                <input type="password" name="passwd" class="check">
-                            </P>
-                        </div>
-                        <button type="submit" class="red-button">予約キャンセル</button>
-                    </form>
+                                <?php $counter = 1;foreach ($results as $row):?>
+                                    <tr>
+                                        <th><?= $counter; ?></th>
+                                        <th><?= htmlspecialchars($row['day']); ?></th>
+                                        <th><?= htmlspecialchars($row['time']); ?>時～</th>
+                                        <th>
+                                            <input type="checkbox" name="selected[]" value="<?= $counter; ?>">
+                                            <input type="hidden" name="day_<?= $counter; ?>" value="<?= htmlspecialchars($row['day']); ?>">
+                                            <input type="hidden" name="time_<?= $counter; ?>" value="<?= htmlspecialchars($row['time']); ?>">
+                                        </th>
+                                        
+                                    </tr>
+                                <?php $counter++; endforeach; ?>
+                            </table>
+                            <p class="error-font error-font2">※キャンセルしたい予約日を選択してください</p>
+                            <h3>・本人確認</h3>
+                            <div class="check-zone">
+                                <p>
+                                    <h4>・メールアドレス</h4>
+                                    <input type="email" name="mail" class="check">
+                                </P>
+                                <p>
+                                    <h4>・パスワード</h4>
+                                    <input type="password" name="passwd" class="check">
+                                </P>
+                            </div>
+                            <button type="submit" class="red-button">予約キャンセル</button>
+                        </form>
+                    <?php else: ?>
+                        <h3>・現在の予約状況</h3>
+                        <P class="not-cancel">現在予約はありません</p>
+                        <P><a href="../mypage/mypage.php" class="red-button">マイページ</a></p>
+                    <?php endif; ?>
                 </div>
             </div>
         </main>
