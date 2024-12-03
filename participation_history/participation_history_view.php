268299d9fb94a877f0d37af6eed0499c6132d4e4
takuto-takuto <tchitian360@gmail.com>
2024-12-03 14:54:36 +0900

    participation_history_view.phpを改修しました

--- participation_history/participation_history_view.php
+++ participation_history/participation_history_view.php
@@ -12,7 +12,7 @@ $results = $_SESSION['results'];
         <title>参加履歴</title>
     </head>
     <body>
-    <script src="../hamburger.js"></script>
+    <script src="./hamburger.js"></script>
         <header>
             <div class="header-wrap">
                 <img src="../img/image 1.png" alt="Arts_Logo">
@@ -49,33 +49,71 @@ $results = $_SESSION['results'];
             <p class="locatoin"><a href="">ホームページ</a>へ/<a href="../mypage/mypage.php">マイページ</a>へ/参加履歴</p>
             <h1>参加履歴</h1>
             <div class="content-container">
-                <h2>参加情報</h2>
-                <div class="participation">
-                    <h3>・参加履歴</h3>
-                    <p class="delete-history"><a href="./delete_history.php">・<span>履歴をリセット</span>する</a></p>
-                    <table border="1">
-                        <thead>
-                            <tr>
-                                <th>NO.</th>
-                                <th>参加日</th>
-                                <th>参加日時</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        <?php $counter = 1;foreach ($results as $row):?>
-                            <tr>
-                                <th><?php echo $counter; ?></th>
-                                <th><?php echo $row['day']; ?></th>
-                                <th><?php echo $row['time']; ?>時～</th>
-                            </tr>
-                        <?php $counter++; endforeach; ?>
-                        </tbody>
-                    </table>
-                    <diV class="button">
+                <?php if($results): ?>
+                    <h2>参加情報</h2>
+                    <div class="participation">
+                        <h3>・参加履歴</h3>
+                        <p class="delete-history"><button class="openDialog">・<span>履歴をリセット</span>する</button></p>
+                        <dialog class="dialogDemo">
+                            <p>本当に削除しますか？</P>
+                            <div class="chose">
+                                <button class="yes">はい</button>
+                                <button class="no">キャンセル</button>
+                            </div>
+                        </dialog>
+                        <script>
+                            const openBtn = document.querySelector('.openDialog');
+                            const dialogDemo = document.querySelector('.dialogDemo');
+                            const yes = document.querySelector('.yes');
+                            const no = document.querySelector('.no');
+                            //ダイアログを開く
+                            openBtn.addEventListener('click', () => {
+                                dialogDemo.showModal();
+                            });
+                            //ダイアログを閉じる
+                            no.addEventListener('click', () => {
+                                dialogDemo.close();
+                            });
+                            //移動する
+                            yes.addEventListener('click', () => {
+                                location.href = './delete_history.php';
+                            });
+                        </script>
+                        <table border="1">
+                            <thead>
+                                <tr>
+                                    <th>NO.</th>
+                                    <th>参加日</th>
+                                    <th>参加日時</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            <?php $counter = 1;foreach ($results as $row):?>
+                                <tr>
+                                    <th><?php echo $counter; ?></th>
+                                    <th><?php echo $row['day']; ?></th>
+                                    <th><?php echo $row['time']; ?>時～</th>
+                                </tr>
+                            <?php $counter++; endforeach; ?>
+                            </tbody>
+                        </table>
+                        <diV class="button">
                             <p><a href="" class="red-button">予約登録</a></p>
                             <p><a href="../mypage/mypage.php" class="red-button">マイページ</a></p>
+                        </div>
                     </div>
-                </div>
+                <?php else: ?>
+                    <h2>参加情報</h2>
+                    <div class="participation">
+                        
+                        <h3>参加履歴</h3>
+                        <p class="none-participation">・現在参加履歴はありません</p>
+                        <diV class="button">
+                            <p><a href="" class="red-button">予約登録</a></p>
+                            <p><a href="../mypage/mypage.php" class="red-button">マイページ</a></p>
+                        </div>
+                    </div>
+                <?php endif; ?>        
             </div>
         </main>
     </body>
