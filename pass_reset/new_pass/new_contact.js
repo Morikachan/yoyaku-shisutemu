c0bf8bffc48fd5fcf470c39397e9a882551f9ffa
haruto-sugihara <sugihr-528@yahoo.co.jp>
2024-12-17 15:08:14 +0900

    パスワードリセットサイトの更新をしました。

--- pass_reset/new_pass/new_contact.js
+++ pass_reset/new_pass/new_contact.js
@@ -4,12 +4,12 @@ const alert_message =document.querySelector('.alert_message');
 const alert_message2 =document.querySelector('.alert_message2');
 const body = document.querySelector("body");
 const button = document.getElementById('sendButton');
+const input = document.querySelector('.input');
 button.style.backgroundColor = '#888888';
 button.style.color = '#000000';
 
 reset_pass.addEventListener('change', function pass(event) {
     newPass();
-    countPass();
 });
 repeat_pass.addEventListener('change', function pass(event){
     newPass();
@@ -18,27 +18,40 @@ function newPass(){
 
     const repeatValue = repeat_pass.value;
     const resetValue = reset_pass.value;
-    let countPass = length(resetValue);
+    const countPass = resetValue.length;
+    const countPass2 = repeat_pass.length;
+    console.log(repeatValue);
+    console.log(resetValue);
     if(resetValue && repeatValue){
-        if(resetValue === repeatValue && countPass >= 8){
+        if(resetValue === repeatValue){
+            if((countPass < 8 )||(countPass2 < 8)){
+                //８文字以下の場合
+                alert_message2.classList.add('active');
+                input.classList.add('active');
+                button.style.backgroundColor = '#888888';
+                button.style.color = '#000000';
+                button.disabled = true;
+            } else {
+
             //一致した場合
             alert_message.classList.remove('active');
             alert_message2.classList.remove('active');
+            input.classList.remove('active');
             button.style.backgroundColor = '#CF220E';
             button.style.color = '#ffffff';
             button.disabled = false;
-        } else {
-            //不一致の場合
-            if(resetValue !== repeatValue){
-                alert_message.classList.add('active');
-            } else {
-                alert_message2.classList.add('active');
             }
+        } else {
+           
+            //不一致の場合
+            alert_message.classList.add('active');
+            input.classList.add('active');
             button.style.backgroundColor = '#888888';
             button.style.color = '#000000';
             button.disabled = true;
         }
     } else  {
+        
         //例外処理
         button.style.backgroundColor = '#888888';
         button.style.color = '#000000';
@@ -46,24 +59,3 @@ function newPass(){
     }
     
 }
-//  function countPass(){
-//     const resetValue = reset_pass.value;
-//     let countPass = length(resetValue);
-//     if (countPass <= 8){
-//         //不一致の場合
-//         alert_message2.classList.add('active');
-//         button.style.backgroundColor = '#888888';
-//         button.style.color = '#000000';
-//         button.disabled = true;
-//     } else {
-//         alert_message2.classList.remove('active');
-//         button.style.backgroundColor = '#CF220E';
-//         button.style.color = '#ffffff';
-//         button.disabled = false;
-//     }
-//  }
-
-// function insertConfirm() {
-//         body.style.overflow = 'hidden';
-//         modal.style.display = 'block';
-// } 
