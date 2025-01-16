cf04f91602b4bef98b66527600a3f49f4a94d18e
haruto-sugihara <sugihr-528@yahoo.co.jp>
2025-01-16 15:12:36 +0900

    細かい文章を修正しました。

--- pass_reset/new_pass/new_contact.php
+++ pass_reset/new_pass/new_contact.php
@@ -173,7 +173,7 @@ if(searchToken($pdo, $passwordResetToken)){
                 <div class="alert_message">・パスワードが一致しません</div>
                 <div class="alert_message2">・8文字以上で設定してください</div>
                 <div class="alert_message3">・24文字以下で設定してください</div>
-                <div class="alert_message4">・大文字と小文字、数字と記号をそれぞれ１つ以上パスワードにいれてください。</div>
+                <div class="alert_message4">・大文字と小文字、数字をそれぞれ１つ以上パスワードにいれてください。</div>
                 <form action="./new_pass.php" method="POST">
                     <label>
                         <h3>新しいパスワード</h3>
