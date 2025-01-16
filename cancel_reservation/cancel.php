b29ba013efc13d96755af3d859f703e6a2d75c8f
takuto-takuto <tchitian360@gmail.com>
2025-01-16 06:11:45 +0000

    cancel.phpレスポンシブ対応のためのクラス追加

--- cancel_reservation/cancel.php
+++ cancel_reservation/cancel.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$results = $_SESSION['results'];
+$results_mypage = $_SESSION['results_mypage'];
 ?>
 
 <!DOCTYPE html>
@@ -16,11 +16,32 @@ $results = $_SESSION['results'];
             <div class="header-wrap">
                 <img src="../img/image 1.png" alt="Arts_Logo">
             </div>
-            <div class="header-wrap">
+            <!-- ロゴを除くオブジェクトを右に固定するためのdiv -->
+            <div class="flex_header">    
                 
-                <!-- ハンバーガーメニュー部分 -->
-                
-                <!--ここまででハンバーガメニュー終わり-->
+                  <!-- ハンバーガメニューのリスト -->
+                  <ul class="c-header__list c-hamburger-menu__list" id="hamburger-menu_list"><!-- 追記 クラスを追記 -->
+                      <li class="c-header__list-item">
+                        <a href="https://www.kccollege.ac.jp/" class="c-header__list-link">ホームページへ</a>
+                      </li>
+                      <li class="c-header__list-item">
+                        <a href="#" class="c-header__list-link">参加履歴</a>
+                      </li>
+                      <li class="c-header__list-item">
+                        <a href="#" class="c-header__list-link">登録内容の変更</a>
+                      </li>
+                      <li class="c-header__list-item">
+                        <a href="#" class="c-header__list-link">アカウント削除</a>
+                      </li>
+                      <li class="c-header__list-item">
+                        <a href="#" class="c-header__list-link">お問い合わせ</a>
+                      </li>
+                  </ul>
+                  
+                  <!-- 新規登録ボタン -->
+                  <a href="#" class="red-button">新規登録</a>
+                  <!-- ハンバーガボタン -->
+                  <div id="hamburger-btn" class="open" onclick="hamburgerClick()"></div>
             </div>
         </header>
         <main>
@@ -29,7 +50,7 @@ $results = $_SESSION['results'];
             <div class="content-container">
                 <h2>予約情報</h2>
                 <div class="Reservation">
-                    <?php if($results): ?>
+                    <?php if($results_mypage): ?>
                         <h3>・現在の予約状況</h3>
                         <form action="./check_cancel.php" method="post">
                             <table border="1">
@@ -41,12 +62,12 @@ $results = $_SESSION['results'];
                                         <th>キャンセル</th>
                                     </thead>
                                 </tr>
-                                <?php $counter = 1;foreach ($results as $row):?>
+                                <?php $counter = 1;foreach ($results_mypage as $row):?>
                                     <tr>
-                                        <th><?= $counter; ?></th>
-                                        <th><?= htmlspecialchars($row['day']); ?></th>
-                                        <th><?= htmlspecialchars($row['time']); ?>時～</th>
-                                        <th>
+                                        <th class="no"><?= $counter; ?></th>
+                                        <th class="day"><?= htmlspecialchars($row['day']); ?></th>
+                                        <th class="time"><?= htmlspecialchars($row['time']); ?>時～</th>
+                                        <th  class="cancel">
                                             <input type="checkbox" name="selected[]" value="<?= $counter; ?>">
                                             <input type="hidden" name="day_<?= $counter; ?>" value="<?= htmlspecialchars($row['day']); ?>">
                                             <input type="hidden" name="time_<?= $counter; ?>" value="<?= htmlspecialchars($row['time']); ?>">
@@ -66,8 +87,8 @@ $results = $_SESSION['results'];
                                     <input type="password" name="passwd" class="check">
                                 </P>
                             </div>
-                            <button type="submit" class="red-button">予約キャンセル</button>
-                            <p><a href="../mypage/mypage.php" class="red-button">マイページ</a></p>
+                            <button type="submit" class="red-button cancel-button">予約キャンセル</button>
+                            <p><a href="../mypage/mypage.php" class="red-button mypage-move">マイページ</a></p>
                         </form>
                     <?php else: ?>
                         <h3>・現在の予約状況</h3>
