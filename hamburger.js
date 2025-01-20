10af46f31279b3c72bef1771f9feff36a9e7bc24
✿ オレクサンドラ ✿ <zuzukochkame@gmail.com>
2025-01-20 11:33:58 +0900

    カレンダーの土日祝表示変恋、トップページのボタン増加、css・jsのそれぞれクイックフィックス

--- hamburger.js
+++ hamburger.js
@@ -1,8 +1,9 @@
 function hamburgerClick(){
     const elem = document.getElementById("hamburger-btn");
     const hamburger_menu_list = document.getElementById("hamburger-menu_list");
-    const main = document.getElementById("main");
-    const body = document.getElementById("body");
+    const main = document.querySelector("main");
+    const body = document.querySelector("body");
+    const toTopButton = document.querySelector('#page-top');
     
    if(hamburger_menu_list.classList.contains('active')){
         hamburger_menu_list.classList.remove('active');
@@ -10,12 +11,14 @@ function hamburgerClick(){
         body.classList.remove('active');
         elem.classList.remove('close');
         elem.classList.add('open');
+        toTopButton.style.zIndex = 1;
    } else {
         hamburger_menu_list.classList.add('active');
         main.classList.add('active');
         body.classList.add('active');
         elem.classList.remove('open');
         elem.classList.add('close');
+        toTopButton.style.zIndex = -1;
    }
    // closeからopenにします
    // closeの時 -> opacity: 0, transform: (-100%, 0);
