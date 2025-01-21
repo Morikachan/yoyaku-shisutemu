1e54e27769255261bc1e5a63ff8fc591d55f76db
✿ オレクサンドラ ✿ <zuzukochkame@gmail.com>
2025-01-21 13:06:27 +0900

    カレンダー表示色の変更、ログインページのメニューのナビゲーションリング調整

--- login.php
+++ login.php
@@ -77,16 +77,16 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
                         <a href="https://www.kccollege.ac.jp/" class="c-header__list-link">ホームページへ</a>
                       </li>
                       <li class="c-header__list-item">
-                        <a href="#" class="c-header__list-link">参加履歴</a>
+                        <a href="./participation_history/participation_history.html" class="c-header__list-link">参加履歴</a>
                       </li>
                       <li class="c-header__list-item">
                         <a href="./change_Information/view/change_Information.php" class="c-header__list-link">登録内容の変更</a>
                       </li>
                       <li class="c-header__list-item">
-                        <a href="http://localhost/yoyaku-shisutemu\delete_account\html\delete.html" class="c-header__list-link">アカウント削除</a>
+                        <a href="./delete_account/html/delete.html" class="c-header__list-link">アカウント削除</a>
                       </li>
                       <li class="c-header__list-item">
-                        <a href="http://localhost/yoyaku-shisutemu/inquiry/inquiry.html" class="c-header__list-link">お問い合わせ</a>
+                        <a href="./inquiry/inquiry.html" class="c-header__list-link">お問い合わせ</a>
                       </li>
                   </ul>
                   
