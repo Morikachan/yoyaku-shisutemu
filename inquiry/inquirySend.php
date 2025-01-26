0071232d03b81ff1edcac18c145b6221043c6ede
haruto-sugihara <sugihr-528@yahoo.co.jp>
2025-01-27 08:57:14 +0900

    お問い合わせフォームのコードを変更しました。追加したコードはコメントアウトしています。

--- inquiry/inquirySend.php
+++ inquiry/inquirySend.php
@@ -1,46 +1,32 @@
 <?php
 
-//inquiry.htmlの実行後に走る処理です。
+//inquiry.phpの実行後に走る処理です。
 //メールの送信を行っています。
 $mail = $_POST['inquiryEmail'];
 $content  = $_POST['inquiryContent'];
+//$sendto = 'k248007@kccollege.ac.jp';    //宛先
+//$sendto2 = $mail; 
+//$body = mailbody('本文です');
+//$header = meilheader('headerです');
+//$sendsubject = "{$mail}様からお問い合わせです"; //件名
+//$maintext = "{$mail}様から。\r\n お問い合わせ内容 \r\n {$content}" . "\n";  //メッセージ
 
-// Setting($mail,$content,$sendto,$maintext,$sendsubject);
+
+// Setting($mail,$content,$sendto,$sendsubject,$maintext);
 
 
-// function Setting($mail,$content,$sendto,$maintext,$sendsubject){
+// function Setting($mail,$content,$maintext){
 //     try {
-//         $IMGNAME = isset($_FILES['avatar']['name']) ? $_FILES['avatar']['name'] : NULL;
-//         $IMGTMP = isset($_FILES['avatar']['tmp_name']) ? $_FILES['avatar']['tmp_name'] : NULL;
-//         $fp = @fopen($IMGTMP, "rb");
-//         $img = @fread($fp, filesize($IMGTMP));
-//         @fclose($fp);
-//         $IMGSIZE = floor(strlen($img) / 1024);
-//         $ENCODEIMG = base64_encode($img);
-//         $imginfo = @getimagesize('data:application/octet-stream;base64,' . $ENCODEIMG);
-//         $IMGTYPE = $imginfo['mime'];
-//         $BOUNDARY = '__BOUNDARY__' .md5(rand());
-//         $to = 'k248007@kccollege.ac.jp';
-//         $subject = "{$mail}様からお問い合わせです";
-//         $header = implode("\r\n",array(
-//             'Content-Type: multipart/mixed;boundary=' . $BOUNDARY ,
-//             'From: webmaster@example.com',
-//             'Reply-To: webmaster@example.com',
-//         ));
-//         $body = "--" . $BOUNDARY . "\n";
-//         $body .= 'Content-Type: text/plain; charset="ISO-2022-JP' . "\n";
-//         $body .= "{$mail}様から。\r\n お問い合わせ内容 \r\n {$content}" . "\n";
-//         $body .= '--' . $BOUNDARY . "\n";
-//         $body .= 'Content-Type: ' . $IMGTYPE . '; name=' . $IMGNAME . "\n";
-//         $body .= 'Content-Disposition: attachment; filename=' . $IMGNAME . "\n";
-//         $body .= 'Content-Transfer-Encoding: base64' . "\n";
-//         $body .= chunk_split($ENCODEIMG) . "\n";
-//         $body .= '--' . $BOUNDARY . '--';
+
+            
 //                 //メールの送信
 //         if(mail($to, $subject,$body,$header)) {
-//             yourmailSetting($mail,$content);
+//             $sendto = $mail;
+//             $sendsubject = "お問い合わせを受け付けました";
+//             $maintext = "以下の内容でお問い合わせを受け付けました。\r\n お問い合わせ内容 \r\n {$content}\n 担当者が確認次第ご連絡させていただきますので少々お待ちください". "\n";
+//             Setting($mail,$content,$sendto,$sendsubject,$maintext);
 //         } else {
-//             header("Location:http://localhost/yoyaku-shisutemu/inquiry/views/mail_error.html");
+//             header("Location: ./views/mail_error.html");
 //             exit();
 //         }
 //     } catch (Exception $e) {
@@ -48,6 +34,45 @@ $content  = $_POST['inquiryContent'];
 //     }
 
 // }
+
+// function mailbody($mail,$content,$maintext){
+//     $IMGNAME = isset($_FILES['avatar']['name']) ? $_FILES['avatar']['name'] : NULL;
+//     $IMGTMP = isset($_FILES['avatar']['tmp_name']) ? $_FILES['avatar']['tmp_name'] : NULL;
+//     $fp = @fopen($IMGTMP, "rb");
+//     $img = @fread($fp, filesize($IMGTMP));
+//     @fclose($fp);
+//     $IMGSIZE = floor(strlen($img) / 1024);
+//     $ENCODEIMG = base64_encode($img);
+//     $imginfo = @getimagesize('data:application/octet-stream;base64,' . $ENCODEIMG);
+//     $IMGTYPE = $imginfo['mime'];
+//     $BOUNDARY = '__BOUNDARY__' .md5(rand());
+
+
+//     $body = "--" . $BOUNDARY . "\n";
+//     $body .= 'Content-Type: text/plain; charset="ISO-2022-JP' . "\n";
+
+//     //差分３
+//     $body .= $maintext;
+
+
+//     $body .= '--' . $BOUNDARY . "\n";
+//     $body .= 'Content-Type: ' . $IMGTYPE . '; name=' . $IMGNAME . "\n";
+//     $body .= 'Content-Disposition: attachment; filename=' . $IMGNAME . "\n";
+//     $body .= 'Content-Transfer-Encoding: base64' . "\n";
+//     $body .= chunk_split($ENCODEIMG) . "\n";
+//     $body .= '--' . $BOUNDARY . '--';
+//     return $body;
+// }
+// function meilheader(){
+//     $BOUNDARY = '__BOUNDARY__' .md5(rand())
+//     $header = implode("\r\n",array(
+//     'Content-Type: multipart/mixed;boundary=' . $BOUNDARY ,
+//     'From: webmaster@example.com',
+//     'Reply-To: webmaster@example.com',
+//     ));
+//     return $header;
+// }
+        
 
 //メールの設定
 
